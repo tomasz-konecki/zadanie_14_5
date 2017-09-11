d8acfae3a7788d8e76001cd05ce5bc001d5af2d1
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-09-11 14:26:45 +0200

    Update Contact.js and style.css

--- components/Contact.js
+++ components/Contact.js
@@ -9,14 +9,18 @@ var Contact = React.createClass({
                     className: 'contact-item'
                 },
 
-                React.createElement('div', {className: 'contact-picture'},
+                React.createElement('div', {
+                        className: 'contact-picture'
+                    },
                     React.createElement('img', {
                         className: 'contact-image',
                         src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
                     })
                 ),
 
-                React.createElement('div', {className: 'contact-details'},
+                React.createElement('div', {
+                        className: 'contact-details'
+                    },
                     React.createElement('p', {
                         className: 'contact-name'
                     }, this.props.item.firstName),
@@ -31,8 +35,13 @@ var Contact = React.createClass({
                     )
                 ),
 
-                React.createElement('div', {className: 'contact-icon'}, 
-                    React.createElement('i', {className: 'fa fa-info-circle', 'aria-hidden': "true"})
+                React.createElement('div', {
+                        className: 'contact-icon'
+                    },
+                    React.createElement('i', {
+                        className: 'fa fa-info-circle',
+                        'aria-hidden': "true"
+                    })
                 )
             )
         )
